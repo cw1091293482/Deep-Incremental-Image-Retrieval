8a7576b79a83b4dd777fe54be36ecf0175809bdf
cw1091293482 <1091293482@qq.com>
2021-02-13 14:27:25 +0100

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,23 @@
+from .BN_Inception import BN_Inception
+
+__factory = {
+    'BN_Inception': BN_Inception
+}
+
+def names():
+    return sorted(__factory.keys())
+
+
+def create(name, *args, **kwargs):
+    """
+    Create a loss instance.
+
+    Parameters
+    ----------
+    name : str
+        the name of loss function
+    """
+    if name not in __factory:
+        raise KeyError("Unknown network:", name)
+
+    return __factory[name](*args, **kwargs)
